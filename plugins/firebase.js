6666fd501727297bde626242568dd53bac3dfe34
Ken Lee <kenlee0852@gmail.com>
2021-08-18 21:02:43 +0800

    add support load firebase config from process env

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,9 +3,23 @@ import "firebase/analytics";
 import "firebase/firestore";
 import "firebase/storage";
 
-import firebaseConfig from './firebase.config';
+let firebaseConfig = null;
+try {
+    firebaseConfig = require('./firebase.json');
+} catch (error) {
+    // load from process env
+    firebaseConfig = {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID
+    }
+}
 
-// 確認firebase是否準備初始化
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
